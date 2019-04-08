3bb65f40060ff5dc02d80a429df53352faec256c
akoita@vagrant@ubuntu-xenial <aboubakar.koita@yahoo.fr>
2019-04-08 08:28:22 -0700

    gitignore - ignore build output files

--- chaincode-candidate/packages/common-cc/dist/src/abstractTrainingParticipant.model.d.ts
+++
@@ -1,6 +0,0 @@
-import { ConvectorModel } from '@worldsibu/convector-core-model';
-export declare abstract class AbstractTrainingParticipant<T extends AbstractTrainingParticipant<any>> extends ConvectorModel<T> {
-    id: string;
-    created: number;
-    modified: number;
-}
--- chaincode-candidate/packages/common-cc/dist/src/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var tslib_1 = require("tslib");
-tslib_1.__exportStar(require("./abstractTrainingAsset.model"), exports);
-tslib_1.__exportStar(require("./abstractTrainingParticipant.model"), exports);
-tslib_1.__exportStar(require("./common.controller"), exports);
-tslib_1.__exportStar(require("./common.model"), exports);
-//# sourceMappingURL=index.js.map
