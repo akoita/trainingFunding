bbf7c419a763347a09f353e3832dc6bb38b64389
akoita@vagrant@ubuntu-xenial <aboubakar.koita@yahoo.fr>
2019-04-09 21:41:04 +0200

    Enum validation with yup For example: @Validate(yup.string().oneOf(Object.keys(TrainingAppLifecycleStatus).map(k => TrainingAppLifecycleStatus[k])))

--- packages/training-cc/src/training.model.ts
+++ packages/training-cc/src/training.model.ts
@@ -1,43 +1,53 @@
 import * as yup from 'yup';
-import {
-  ConvectorModel,
-  Default,
-  ReadOnly,
-  Required,
-  Validate
-} from '@worldsibu/convector-core-model';
-import {AbstractTrainingAsset} from 'common-cc';
+import {ReadOnly, Required, Validate} from '@worldsibu/convector-core-model';
+import {AbstractTrainingAsset, TrainingAppLifecycleStatus} from 'common-cc';
 import {TrainingOffer} from 'trainingOffer-cc';
 import {Candidate} from 'candidate-cc';
 
-export class Training extends AbstractTrainingAsset<Training> {
 
-  @ReadOnly()
-  @Required()
-  public readonly type = 'io.worldsibu.training';
-
-  @ReadOnly()
-  @Required()
-  @Validate(TrainingOffer)
-  public trainingOffer: TrainingOffer;
-
-  @Required()
-  public trainingProcessStatus: TrainingProcessStatus;
-
-  @ReadOnly()
-  @Required()
-  @Validate(Candidate)
-  public candidate: Candidate;
-
+export enum TrainingProcessStatus {
+    NotSubmitted,
+    Submitted,
+    Funded,
+    InProgress,
+    Succeeded,
+    Failed,
+    Accepted
 }
 
 
-export enum TrainingProcessStatus {
-  NotSubmitted,
-  Submitted,
-  Funded,
-  InProgress,
-  Succeeded,
-  Failed,
-  Accepted
+export class Training extends AbstractTrainingAsset<Training> {
+
+    @ReadOnly()
+    @Required()
+    public readonly type = 'io.worldsibu.training';
+
+    @ReadOnly()
+    @Required()
+    @Validate(TrainingOffer)
+    public trainingOffer: TrainingOffer;
+
+    @Required()
+    @Validate(yup.string().oneOf(Object.keys(TrainingProcessStatus).map(k => TrainingProcessStatus[k])))
+    public trainingProcessStatus: TrainingProcessStatus;
+
+    @ReadOnly()
+    @Required()
+    @Validate(Candidate)
+    public candidate: Candidate;
+
+    public static build(value: {
+        id: string, created: number, modified: number, status: TrainingAppLifecycleStatus, trainingOffer: TrainingOffer,
+        trainingProcessStatus: TrainingProcessStatus, candidate: Candidate
+    }): Training {
+        const model = new Training();
+        model.id = value.id;
+        model.created = value.created;
+        model.modified = value.modified;
+        model.status = value.status;
+        model.trainingOffer = value.trainingOffer;
+        model.trainingProcessStatus = value.trainingProcessStatus;
+        model.candidate = value.candidate;
+        return model;
+    }
 }
