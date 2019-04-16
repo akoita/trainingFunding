4dd93f6aef610ea0e1f1c3df6a09d734dc37711a
akoita@vagrant@ubuntu-xenial <aboubakar.koita@yahoo.fr>
2019-04-17 01:42:16 +0200

    Loopback - backend

---
+++ packages/webapp/src/index.ts
@@ -0,0 +1,16 @@
+import {WebappApplication} from './application';
+import {ApplicationConfig} from '@loopback/core';
+
+export {WebappApplication};
+
+export async function main(options: ApplicationConfig = {}) {
+  const app = new WebappApplication(options);
+  await app.boot();
+  await app.start();
+
+  const url = app.restServer.url;
+  console.log(`Server is running at ${url}`);
+  console.log(`Try ${url}/ping`);
+
+  return app;
+}
